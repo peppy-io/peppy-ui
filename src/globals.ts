18091024e6f79020531e0c125e0a6a3ab17b364f
Samyak S Sarnayak <samyak201@gmail.com>
2021-11-13 23:46:31 +0530

    fix: SSR issues in globals.ts

--- src/globals.ts
+++ src/globals.ts
@@ -1,15 +1,16 @@
 import { readable, writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
+import { browser } from '$app/env';
 
 export const apiBaseUrl = readable("http://localhost:4000");
 
 // JWT token - persist in local storage
 export let token: Writable<string>;
-if (typeof process === 'undefined' || process.browser) {
+if (browser) {
   token = writable(window.localStorage.getItem("token") ?? "");
   token.subscribe((val: string) => window.localStorage.setItem("token", val));
 } else {
   token = writable("");
-  token.subscribe((val: string) => window.localStorage.setItem("token", val));
+  token.subscribe((val: string) => browser && window.localStorage.setItem("token", val));
 }
 
