4ade4bb9dbc9069f595186b489063a749f4078ab
Samyak S Sarnayak <samyak201@gmail.com>
2021-11-14 11:58:44 +0530

    fix: use api base url from .env

--- src/globals.ts
+++ src/globals.ts
@@ -2,7 +2,7 @@ import { readable, writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 import { browser } from '$app/env';
 
-export const apiBaseUrl = readable("http://localhost:4000");
+export const apiBaseUrl = readable(import.meta.env.VITE_API_BASE_URL ?? "https://peppy-backend.herokuapp.com");
 
 // JWT token - persist in local storage
 export let token: Writable<string>;
