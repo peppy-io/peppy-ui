252772a51cb4d29500717f743624f4f4196d21f1
Samyak S Sarnayak <samyak201@gmail.com>
2021-11-14 12:41:51 +0530

    feat: add example line graph using chart.js

--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -1,16 +1,96 @@
 <script lang="ts">
 	import { token } from '../globals';
 	import { goto } from '$app/navigation';
+	import { Line } from 'svelte-chartjs';
 
-        // TODO: validate token from backend here
-        if ($token === "") {
-            goto("/login");
-        }
+	// TODO: validate token from backend here
+	if ($token === '') {
+		goto('/login');
+	}
+
+	const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: "Today's energy level",
+				backgroundColor: 'rgb(255, 99, 132)',
+				borderColor: 'rgb(255, 99, 132)',
+				data: [5, 10, -5, 0, 5, 10],
+				pointBorderWidth: 10,
+				pointHoverRadius: 5,
+				lineTension: 0.3,
+				color: '#fff'
+			},
+			{
+				label: 'Baseline',
+				data: Array(6).fill(0),
+				backgroundColor: 'rgb(100, 100, 100, 0.5)',
+				borderColor: 'rgb(100, 100, 100, 0.5)',
+				color: '#fff'
+			}
+		]
+	};
+
+        const fontSize = 15;
+        const fontColor = 'white';
+
+	const options = {
+		responsive: true,
+		scales: {
+			x: {
+				ticks: {
+					color: fontColor
+				},
+
+				font: {
+					size: fontSize
+				},
+				title: {
+					color: fontColor
+				}
+			},
+			y: {
+				ticks: {
+					color: fontColor
+				},
+				font: {
+					size: fontSize
+				},
+				title: {
+					color: fontColor
+				}
+			}
+		},
+		grid: {
+			font: {
+				size: fontSize
+			},
+			color: fontColor
+		},
+		title: {
+			font: {
+				size: fontSize
+			},
+			color: fontColor
+		},
+		plugins: {
+			legend: {
+				labels: {
+					font: {
+						size: fontSize
+					},
+					color: fontColor
+				}
+			}
+		}
+	};
 </script>
 
 <main class="md:container md:mx-auto px-4">
-	<h1>Here lies your dashboard</h1>
-        <p>token: {$token}</p>
+	<h1 class="text-3xl">Your energy levels for today:</h1>
 
-        <!-- TODO: show nice graphs, form to log a task -->
+	<!-- TODO: get data from api, form to log a task -->
+
+	<Line {data} {options} />
 </main>
